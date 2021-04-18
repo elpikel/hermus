7ad56f6fd2f7d354a309f649b7273a520f05ef95
Arkadiusz Plichta <arkadiusz@blockfi.com>
2021-04-18 19:41:31 +0200

    removed sass

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,4 +1,3 @@
-import "../css/app.scss"
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
--- web/assets/webpack.config.js
+++ web/assets/webpack.config.js
@@ -39,8 +39,7 @@ module.exports = (env, options) => {
           use: [
             MiniCssExtractPlugin.loader,
             'css-loader',
-            'postcss-loader',
-            'sass-loader',
+            'postcss-loader'
           ],
         }
       ]
